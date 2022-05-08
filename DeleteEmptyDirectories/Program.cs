ab3e43cff47d5726f9006cd8a2de6150fadfdec5
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2022-05-08 11:46:48 +0200

    update

---
+++ DeleteEmptyDirectories/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteEmptyDirectories
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            DeleteEmptyDirsInDirectory(Directory.GetCurrentDirectory());
+            //DeleteEmptyDirsInDirectory(@"D:\Pictures\");
+        }
+
+        private static void DeleteEmptyDirsInDirectory(string dir)
+        {
+            foreach (string path in Directory.GetDirectories(dir))
+            {
+                if (!PathHasContent(path))
+                {
+                    FileAttributes attr = File.GetAttributes(path);
+
+                    if (!attr.HasFlag(FileAttributes.Directory)) throw new Exception("Non directory path got marked for deletion!");
+
+                    Console.WriteLine($"Removing {path}");
+                    Directory.Delete(path, false);
+                }
+            }
+        }
+
+        private static bool PathHasContent(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                if (!PathHasContent(dir)) DeleteEmptyDirsInDirectory(path);
+                else files = new string[] { dir };
+            }
+
+            //if (files.Length != 0) return true;
+            return files.Length != 0;
+
+            //return false;
+        }
+    }
+}
---
+++ Finder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finder
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Please input file path: ");
+            string path = Console.ReadLine().Replace("\"", "").Trim();
+            Console.WriteLine("Please input search text: ");
+            string searchText = Console.ReadLine().Trim();
+
+            List<string> list = new List<string>();
+            using (StreamReader sr = new(path))
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (line.Contains(searchText))
+                        list.Add(line);
+                }
+
+            using (StreamWriter sw = new("output.txt"))
+                foreach(string s in list)
+                    sw.WriteLine(s);
+
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = Path.GetFullPath("output.txt")
+            };
+            Process.Start(psi);
+        }
+    }
+}
